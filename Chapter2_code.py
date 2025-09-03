7eb4f45f9dcb9d672393456deaaeebc66330dad2
HamzaC-CS <cheema.hamba@gmail.com>
2025-09-03 10:13:02 -0700

    Create Chapter2_code.py

---
+++ Chapter2_code.py
@@ -0,0 +1,33 @@
+Manual Linear Regression
+
+def forward_linear_regression(X_batch: ndarray,
+                              y_batch: ndarray,
+                              weights: Dict[str, ndarray]
+                              )-> Tuple[float, Dict[str, ndarray]]:
+    '''
+    Forward pass for the step-by-step linear regression.
+    '''
+    # assert batch sizes of X and y are equal
+    assert X_batch.shape[0] == y_batch.shape[0]
+
+    # assert that matrix multiplication can work
+    assert X_batch.shape[1] == weights['W'].shape[0]
+
+    # assert that B is simply a 1x1 ndarray
+    assert weights['B'].shape[0] == weights['B'].shape[1] == 1
+
+    # compute the operations on the forward pass
+    N = np.dot(X_batch, weights['W'])
+
+    P = N + weights['B']
+
+    loss = np.mean(np.power(y_batch - P, 2))
+
+    # save the information computed on the forward pass
+    forward_info: Dict[str, ndarray] = {}
+    forward_info['X'] = X_batch
+    forward_info['N'] = N
+    forward_info['P'] = P
+    forward_info['y'] = y_batch
+
+    return loss, forward_info

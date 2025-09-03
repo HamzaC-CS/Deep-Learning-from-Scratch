20193ae1b667a2eeb074347d5b5638654860d27c
HamzaC-CS <cheema.hamba@gmail.com>
2025-09-03 11:31:17 -0700

    Update Chapter2_code.py

--- Chapter2_code.py
+++ Chapter2_code.py
@@ -31,3 +31,58 @@ def forward_linear_regression(X_batch: ndarray,
     forward_info['y'] = y_batch
 
     return loss, forward_info
+
+def to_2d_np(a: ndarray, 
+             type: str = "col") -> ndarray:
+    '''
+    Turns a 1D Tensor into 2D
+    '''
+
+    assert a.ndim == 1, \
+    "Input tensors must be 1 dimensional"
+    
+    if type == "col":        
+        return a.reshape(-1, 1)
+    elif type == "row":
+        return a.reshape(1, -1)
+
+def permute_data(X: ndarray, y: ndarray):
+    '''
+    Permute X and y, using the same permutation, along axis=0
+    '''
+    perm = np.random.permutation(X.shape[0])
+    return X[perm], y[perm]
+
+def loss_gradients(forward_info: Dict[str, ndarray],
+                   weights: Dict[str, ndarray]) -> Dict[str, ndarray]:
+    '''
+    Compute dLdW and dLdB for the step-by-step linear regression model.
+    '''
+    batch_size = forward_info['X'].shape[0]
+
+    dLdP = -2 * (forward_info['y'] - forward_info['P'])
+
+    dPdN = np.ones_like(forward_info['N'])
+
+    dPdB = np.ones_like(weights['B'])
+
+    dLdN = dLdP * dPdN
+
+    dNdW = np.transpose(forward_info['X'], (1, 0))
+    
+    # need to use matrix multiplication here,
+    # with dNdW on the left (see note at the end of last chapter)    
+    dLdW = np.dot(dNdW, dLdN)
+
+    # need to sum along dimension representing the batch size:
+    # see note near the end of the chapter    
+    dLdB = (dLdP * dPdB).sum(axis=0)
+
+    loss_gradients: Dict[str, ndarray] = {}
+    loss_gradients['W'] = dLdW
+    loss_gradients['B'] = dLdB
+
+    return loss_gradients
+
+
+

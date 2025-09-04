ed71647c08b0ebcf56ea1a5a19b962aafd0ba5fd
HamzaC-CS <cheema.hamba@gmail.com>
2025-09-04 16:58:39 -0700

    Update Chapter2_code.py

--- Chapter2_code.py
+++ Chapter2_code.py
@@ -117,6 +117,29 @@ Compute root mean squared error.
 '''
 return np.sqrt(np.mean(np.power(preds - actuals, 2)))
 
+def forward_loss(X: ndarray, y: ndarray,
+                     weights: Dict[str, ndarray]
+                     ) -> Tuple[Dict[str, ndarray], float]:
+'''
+Compute the forward pass and the loss for the step-by-step neural network model.
+'''
+M1 = np.dot(X, weights['W1'])
+        N1 = M1 + weights['B1']
+        O1 = sigmoid(N1)
+        M2 = np.dot(O1, weights['W2'])
+        P = M2 + weights['B2']
+        loss = np.mean(np.power(y - P, 2))
+        forward_info: Dict[str, ndarray] = {}
+        forward_info['X'] = X
+        forward_info['M1'] = M1
+        forward_info['N1'] = N1
+        forward_info['O1'] = O1
+        forward_info['M2'] = M2
+        forward_info['P'] = P
+        forward_info['y'] = y
+return forward_info, loss
+
+
 
 
 

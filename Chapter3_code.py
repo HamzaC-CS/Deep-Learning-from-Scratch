e4afb3b22bc68f04aadbe9a51e72c99a0a8cd780
HamzaC-CS <cheema.hamba@gmail.com>
2025-09-05 13:11:17 -0700

    Update Chapter3_code.py

--- Chapter3_code.py
+++ Chapter3_code.py
@@ -78,3 +78,23 @@ class ParamOperation(Operation):
         Every subclass of ParamOperation must implement _param_grad.
         '''
         raise NotImplementedError()
+
+class WeightMultiply(ParamOperation): '''
+        Weight multiplication operation for a neural network.
+        '''
+def __init__(self, W: ndarray): '''
+Initialize Operation with self.param = W. '''
+super().__init__(W)
+def _output(self) -> ndarray: '''
+Compute output.
+'''
+return np.dot(self.input_, self.param)
+def _input_grad(self, output_grad: ndarray) -> ndarray: '''
+Compute input gradient.
+'''
+return np.dot(output_grad, np.transpose(self.param, (1, 0)))
+def _param_grad(self, output_grad: ndarray) -> ndarray: '''
+Compute parameter gradient.
+'''
+return np.dot(np.transpose(self.input_, (1, 0)), output_grad)v
+

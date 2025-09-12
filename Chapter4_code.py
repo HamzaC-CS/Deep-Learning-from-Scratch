d23b4cef730e5453e66371dc4de8a2ad9462711d
HamzaC-CS <cheema.hamba@gmail.com>
2025-09-12 16:20:36 -0700

    Create Chapter4_code.py

---
+++ Chapter4_code.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from lincoln import activations
+from lincoln import layers
+from lincoln import losses
+from lincoln import optimizers
+from lincoln import network
+from lincoln import train
+from lincoln.utils import mnist
+
+RANDOM_SEED = 190119
